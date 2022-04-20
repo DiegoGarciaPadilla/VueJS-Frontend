399409fa6dd9d2d88a1afd6060a819ec5fb26b39
Diego Garcia Padilla <gpdiego370@outlook.com>
2022-04-20 12:20:19 -0500

    Sabores

---
+++ src/components/SaborComponent.vue
@@ -0,0 +1,35 @@
+<template>
+  <li>
+    <img :src="'../assets/' + imgSource" alt="Sabor" width="200px" />
+    <p>{{ sabor }}</p>
+    <p><i>$ {{ precio }}</i></p>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'SaborComponent',
+  props: { sabor: String, precio: String, imgSource: String }
+}
+</script>
+
+<style scoped>
+li {
+  width: 30%;
+  margin: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  transition: all 0.3s ease;
+  cursor: pointer;
+
+  font-size: 20px;
+}
+
+li:hover {
+    transform: scale(1.1);
+    box-shadow: 16px 19px 33px 0px rgba(0,0,0,0.2);
+  }
+</style>
---
+++ src/views/SaboresView.vue
@@ -0,0 +1,51 @@
+<template>
+<div class="contenido">
+  <h2>Sabores</h2>
+  <hr>
+  <ul class="productos">
+    <SaborComponent v-for='sabor in $store.state.pasteles' v-bind:key='sabor' :sabor='sabor.nombre' :precio='sabor.precio' :imgSource='sabor.img' />
+  </ul>
+</div>
+  <FooterComponent />
+</template>
+
+<script>
+// @ is an alias to /src
+import SaborComponent from '@/components/SaborComponent.vue'
+import FooterComponent from '@/components/FooterComponent.vue'
+
+export default {
+  components: { SaborComponent, FooterComponent },
+  name: 'SaboresView'
+}
+</script>
+
+<style scoped>
+.contenido {
+  width: 60%;
+  margin: 50px auto;
+}
+
+.contenido h1 {
+  font-size: 40px;
+}
+
+.contenido h2 {
+  font-size: 28px;
+}
+
+.contenido p {
+  line-height: 30px;
+  text-align: left;
+}
+
+.productos {
+  width: 100%;
+  padding: 10px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  flex-wrap: wrap;
+}
+</style>
