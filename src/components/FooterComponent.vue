2a25a02f2b1d50162220e04c4f24a19d8a31cff9
Diego Garcia Padilla <gpdiego370@outlook.com>
2022-04-20 12:20:33 -0500

    Create FooterComponent.vue

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="footer">
+    <p>Esta es una empresa ficticia hecha para la práctica para la misión de Frontend del bootcamp de LaunchX por Microsoft.</p>
+    <a href="https://github.com/DiegoGarciaPadilla/VueJS-Frontend">Github Repository</a>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'FooterComponent'
+}
+</script>
+
+<style scoped>
+.footer {
+  width: 100%;
+  height: 8vh;
+  margin: 250px auto 0;
+  padding: 30px 0;
+  text-align: center;
+
+  background-color: #f6ecdd;
+}
+</style>
