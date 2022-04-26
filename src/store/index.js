b6d874f5025e643ceab05060dee8edb7ee59e1f1
Diego Garcia Padilla <gpdiego370@outlook.com>
2022-04-25 21:54:40 -0500

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,55 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    pasteles: {
+      cheesecake: {
+        nombre: 'Cheesecake',
+        precio: 200,
+        img: 'cheesecake.png'
+      },
+      tiramisu: {
+        nombre: 'Tiramisú',
+        precio: 200,
+        img: 'tiramisu.png'
+      },
+      tresLeches: {
+        nombre: 'Tres Leches',
+        precio: 200,
+        img: 'tres-leches.png'
+      },
+      opera: {
+        nombre: 'Ópera',
+        precio: 200,
+        img: 'opera.png'
+      },
+      moka: {
+        nombre: 'Moka',
+        precio: 200,
+        img: 'moka.png'
+      },
+      personalizado: {
+        nombre: 'Personalzado',
+        precio: '???',
+        img: 'personalizado.png'
+      }
+    },
+    adornos: {
+      cumpleaños: {
+        nombre: 'Adornos de cumpleaños',
+        precio: 200,
+        img: 'cumpleanos.png'
+      },
+      nupciales: {
+        nombre: 'Adornos nupciales',
+        precio: 200,
+        img: 'boda.png'
+      },
+      personalizado: {
+        nombre: 'Adornos personalizados',
+        precio: 200,
+        img: 'minecraft.jpg'
+      }
+    }
   },
   getters: {
   },
